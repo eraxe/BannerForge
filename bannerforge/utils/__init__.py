a67d81501828c23d417e1be33f05090c652e4c53
Arash Abolhasani <parppl@gmail.com>
2026-06-02 04:07:44 +0330

    chore: remove leftover bannerforge/ folder

--- bannerforge/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .constants import C, Icons, TABS, DEFAULT_SETTINGS
-from .file_io import (ConfigManager, BookmarkManager,
-                      atomic_write_json, get_by_path, set_by_path, del_by_path)
